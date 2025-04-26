bebb539395e0df403d41546414235f4c2b987c97
wongc138 <wongc138@uni.coventry.ac.uk>
2025-04-26 20:08:37 +0100

    children2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ export default function App() {
     <div>
       <Steps />
       {/* <Steps /> */}
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>⚛️ </p>
+      </StepMessage>
     </div>
   );
 }
@@ -49,10 +53,23 @@ function Steps() {
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}:{messages[step - 1]}
-            {/* {test.name} */}
-          </p>
+          {/* <p className="message">
+            <h3>Step {step}</h3>
+          </p> */}
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -76,6 +93,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
